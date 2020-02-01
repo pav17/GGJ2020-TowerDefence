e91b6d793705afecc9e481d4eeb533d20cddcee1
TrickyTriangles <47727993+TrickyTriangles@users.noreply.github.com>
2020-02-01 17:52:58 -0500

    IgnoreFix

--- GGJ2020_TowerDefense/Assets/Scripts/BulletScript.cs
+++ GGJ2020_TowerDefense/Assets/Scripts/BulletScript.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public Vector3 origin;
     public GameObject target;
     public Effect effect;
 
-    public float damage;
+    public int damage;
     private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        origin = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= 1f)
+        {
+            HitTarget();
+        }
+
+        transform.position = Vector3.Lerp(origin, target.transform.position, timer);
+    }
+
+    void HitTarget()
+    {
+        EnemyScript e_script = target.GetComponent<EnemyScript>();
+
+        if (e_script is object) // Null check
+        {
+            e_script.TakeDamage(damage);
+        }
     }
 }
